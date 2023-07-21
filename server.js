7c591831d3dfc0f01b6ba2704cac3a191cbbdbb1
tr-pyramidanatomy <tr.prof06@gmail.com>
2023-07-21 20:45:34 +0200

    add https certificate

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http');
+const https = require('https')
 const app = require('./app');
+const fs = require('fs')
 
 const normalizePort = val => {
 	const port = parseInt(val, 10);
@@ -12,7 +14,7 @@ const normalizePort = val => {
 	}
 	return false;
 };
-const port = normalizePort(process.env.PORT ||'5678');
+const port = normalizePort(process.env.PORT || '5678');
 app.set('port', port);
 
 const errorHandler = error => {
@@ -35,13 +37,28 @@ const errorHandler = error => {
 	}
 };
 
-const server = http.createServer(app);
+const isHTTP = process.env.NODE_ENV !== 'prod'
+
+// const server = http.createServer(app);
+
+// start http or https server
+const server = isHTTP ?
+	// http config
+	http.createServer(app) :
+	// otherwise https config
+	https.createServer({
+		// options https://nodejs.org/api/tls.html#tls_tls_createsecurecontext_options
+		// https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
+		key: fs.readFileSync(path.resolve("/home/ubuntu/live/matouba-portfolio.fr/privkey.pem")),
+		cert: fs.readFileSync(path.resolve("/home/ubuntu/live/matouba-portfolio.fr/cert.pem")),
+		ca: fs.readFileSync(path.resolve("/home/ubuntu/live/matouba-portfolio.fr/chain.pem"))
+	}, app)
 
 server.on('error', errorHandler);
 server.on('listening', () => {
 	const address = server.address();
 	const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-	console.log('Listening on ' + bind);
+	console.log('Listening on ' + bind + ' - process.env.NODE_ENV:' + process.env.NODE_ENV);
 });
 
 server.listen(port);
