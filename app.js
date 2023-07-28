0b57c0aea7230c324bb5c65c1c38fe319143c585
tr-pyramidanatomy <tr.prof06@gmail.com>
2023-07-28 19:58:08 +0200

    enabled CORS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors')
 require('dotenv').config();
-const helmet = require('helmet');
+// const helmet = require('helmet');
 // const swaggerUi = require('swagger-ui-express')
 // const yaml = require('yamljs')
 // const swaggerDocs = yaml.load('swagger.yaml')
@@ -10,10 +10,18 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(helmet({
-  crossOriginResourcePolicy: false,
-}));
+// app.use(helmet({
+//   crossOriginResourcePolicy: false,
+// }));
 app.use('/images', express.static(path.join(__dirname, 'images')))
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
 
 const db = require("./models");
 const userRoutes = require('./routes/user.routes');
