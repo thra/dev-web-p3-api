e524235f817451e7fb84482111778e57f848f1ad
tr-pyramidanatomy <tr.prof06@gmail.com>
2023-08-18 19:22:42 +0200

    update access storage in function of env

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	dialect: "sqlite",
-	storage: '/home/ubuntu/dev-web-p3-api-prod/database.sqlite'
+	storage: process.env.NODE_ENV !== 'prod' ? './database.sqlite' : '/home/ubuntu/dev-web-p3-api-prod/database.sqlite'
 };
