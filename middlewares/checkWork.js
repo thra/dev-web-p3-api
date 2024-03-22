732f4c79e81c3d8528c6a29031ca4fcbc63adf99
tr-pyramidanatomy <tr.prof06@gmail.com>
2024-03-22 08:35:51 +0100

    remove ??

--- middlewares/checkWork.js
+++ middlewares/checkWork.js
@@ -1,25 +1,25 @@
 module.exports = (req, res, next) => {
-	try{
+	try {
 		const host = req.get('host');
-		const title = req.body.title.trim() ?? undefined;
-		const categoryId = parseInt(req.body.category) ?? undefined;
-		const userId = req.auth.userId ?? undefined;
-		const imageUrl = `https://p3-prod-api.matouba-portfolio.fr/api/works/${req.file.filename}` ?? undefined;
-	console.log(title,categoryId,userId,imageUrl)
-		if(title !== undefined &&
+		const title = req.body.title.trim();
+		const categoryId = parseInt(req.body.category);
+		const userId = req.auth.userId;
+		const imageUrl = `https://p3-prod-api.matouba-portfolio.fr/api/works/${req.file.filename}`;
+		console.log(title, categoryId, userId, imageUrl)
+		if (title !== undefined &&
 			title.length > 0 &&
 			categoryId !== undefined &&
 			categoryId > 0 &&
 			userId !== undefined &&
 			userId > 0 &&
-			imageUrl !== undefined){
-			req.work = {title, categoryId, userId, imageUrl}
+			imageUrl !== undefined) {
+			req.work = { title, categoryId, userId, imageUrl }
 			next()
-		}else{
-			return res.status(400).json({error: new Error("Bad Request")})
+		} else {
+			return res.status(400).json({ error: new Error("Bad Request") })
 		}
-	}catch(e){
-		return res.status(500).json({error: new Error("Something wrong occured")})
+	} catch (e) {
+		return res.status(500).json({ error: new Error("Something wrong occured") })
 	}
 
 }
